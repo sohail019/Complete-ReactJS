4ff19a0359d2f58138f7f286d772671a2eec1fdb
sohail019 <shaikhhsohail0193@gmail.com>
2024-08-06 11:50:13 +0530

    Learned about State Hook and how to reflect it on UI

---
+++ 01_Chai_aur_React/03_CounterProject_by_Hooks/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  // let counter = 15
+
+  let [counter, setCounter] = useState(10)
+
+  const addValue = ()=> {
+    if(counter<=20){
+      setCounter(counter++)
+    }
+  }
+
+  const removeValue = () => {
+    if(counter >= 0){
+      setCounter(counter--)
+    }
+  }
+
+
+  return (
+    <>
+      <h1>Chai aur React | Sohail Shaikh</h1>
+
+      <h2>Count Value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value {counter}</button>
+      <button onClick={removeValue}>Remove Value {counter}</button>
+
+      <footer>{counter}</footer>
+    </>
+  );
+}
+
+export default App
