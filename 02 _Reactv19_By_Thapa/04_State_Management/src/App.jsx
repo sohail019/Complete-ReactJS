d114927d8f5a31b616d3675dc4e34086bf158d01
sohail019 <shaikhhsohail0193@gmail.com>
2024-08-11 12:42:38 +0530

    Learned about how State works behind the scene and how it reflects on child component as well as sibling components

--- 02 _Reactv19_By_Thapa/04_State_Management/src/App.jsx
+++ 02 _Reactv19_By_Thapa/04_State_Management/src/App.jsx
@@ -6,8 +6,29 @@ function App() {
   return (
     <>
       <Counter />
+      <SiblingComponent />
     </>
   )
 }
 
 export default App
+
+
+//? Let's create one sibling component here to see how state works
+
+//! When button is clicked in counter, it updates the state of the counter component. This state change triggers a re-render of the counter component. 
+
+const SiblingComponent = () => {
+  
+  console.log("Sibling Component rendered")
+  
+  return (
+    <>
+      <div className='main-div'>
+      <h1>Sibling Component</h1>
+      </div>
+    </>
+
+)
+}
+//* However, the sibling component does not re-render because it does not depend on the state of the counter component. It only depends on the props passed to it by the parent component, which are not updated when the counter component re-renders.
--- 02 _Reactv19_By_Thapa/04_State_Management/src/components/Counter.jsx
+++ 02 _Reactv19_By_Thapa/04_State_Management/src/components/Counter.jsx
@@ -26,6 +26,25 @@ export const Counter = () => {
           <button onClick={handleDecrementCount}>Decrement</button>
         </div>
       </div>
+
+      <ChildComponent count={count}/>
     </>
   );
 };
+
+
+//? Create one child component to see how state works
+//! The child component is rendered when the parent component is rendered. The child component is not re-render when the parent component is re-rendered. However, the child component will be re-rendered when the state of the parent component changes. 
+
+const ChildComponent = (props) => {
+    console.log("Child component rendered")    
+    return (
+        <>
+            <div className="main-div">
+                <h1>Child Component - {props.count}</h1>
+            </div>
+        </>
+    )
+}
+
+//* In this case, the state of the parent component is changed when the handleIncrementCount or handleDecrementCount function is called. So, the child component will be re-rendered when the handleIncrementCount or handleDecrementCount function is called.
