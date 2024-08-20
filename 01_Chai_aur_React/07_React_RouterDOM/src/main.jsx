eb32ea43ccd5ec30b97000c5f1aeaa32b4ca5c12
sohail019 <shaikhhsohail0193@gmail.com>
2024-08-20 19:25:22 +0530

    Learned about React Router DOM and how to create multiple pages and handle it in router

---
+++ 01_Chai_aur_React/07_React_RouterDOM/src/main.jsx
@@ -0,0 +1,35 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App.jsx'
+import { Home } from './components/Home/Home.jsx'
+import { About } from './components/About/About.jsx'
+import { Contact } from './components/Contact/Contact.jsx'
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
