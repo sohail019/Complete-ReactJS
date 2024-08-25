80ceafa78159c9e5ccd27c305ca7d1dd92fd4828
sohail019 <shaikhhsohail0193@gmail.com>
2024-08-25 18:09:58 +0530

    Error handled when data is not fetched

--- 02 _Reactv19_By_Thapa/09_Hook-useEffect/src/components/Pokemon.jsx
+++ 02 _Reactv19_By_Thapa/09_Hook-useEffect/src/components/Pokemon.jsx
@@ -2,59 +2,65 @@ import { useState, useEffect } from "react";
 import "./Pokemon.css";
 
 export const Pokemon = () => {
-    const [pokemon, setPokemon] = useState(null)
-    const [loading, setLoading] = useState(true)
+  const [pokemon, setPokemon] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
   const API = "https://pokeapi.co/api/v2/pokemon/pikachu";
 
   const fetchPokemon = () => {
     fetch(API)
-        .then((res) => res.json())
-        .then((data) => {
-            setPokemon(data);
-            setLoading(false)
-        })
-        .catch((err) => {
-            console.error("Error: ", err);
-            setLoading(false)
-        })
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemon(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("Error: ", err);
+        setError(err)
+        setLoading(false);
+      });
   };
 
   useEffect(() => {
-    fetchPokemon()
+    fetchPokemon();
   }, []);
 
-  if(loading) {
-    return <h1>Loading...</h1>
+  if (loading) {
+    return <h1>Loading...</h1>;
   }
 
-//   if(pokemon) {
-    return (
-      <section className="container">
-        <header>
-          <h1> Lets Catch Pokémon</h1>
-        </header>
-        <ul className="card-demo">
-          <li className="pokemon-card">
-            <figure>
-              <img
-                className="pokemon-image"
-                src={pokemon.sprites.other.home.front_default}
-              />
-            </figure>
-            <h1>Name: {pokemon.name}</h1>
-            <div className="grid-three-cols">
-              <p className="pokemon-info">
-                Height: <span>{pokemon.height} </span>
-              </p>
-              <p className="pokemon-info">
-                Weight: <span>{pokemon.weight}</span>
-              </p>
-              <p className="pokemon-info">
-                speed: <span></span>
-              </p>
-            </div>
-          </li>
-        </ul>
-      </section>
-    );
+  if(error) {
+    return <h1>Error: {error.message}</h1>
+  }
+
+  //   if(pokemon) {
+  return (
+    <section className="container">
+      <header>
+        <h1> Lets Catch Pokémon</h1>
+      </header>
+      <ul className="card-demo">
+        <li className="pokemon-card">
+          <figure>
+            <img
+              className="pokemon-image"
+              src={pokemon.sprites.other.home.front_default}
+            />
+          </figure>
+          <h1>Name: {pokemon.name}</h1>
+          <div className="grid-three-cols">
+            <p className="pokemon-info">
+              Height: <span>{pokemon.height} </span>
+            </p>
+            <p className="pokemon-info">
+              Weight: <span>{pokemon.weight}</span>
+            </p>
+            <p className="pokemon-info">
+              speed: <span>{pokemon.stats[5].base_stat}</span>
+            </p>
+          </div>
+        </li>
+      </ul>
+    </section>
+  );
 };
