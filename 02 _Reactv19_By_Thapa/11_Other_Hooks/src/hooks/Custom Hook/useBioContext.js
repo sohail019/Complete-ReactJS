64dc6677df513e6056e9a3b1b99c6fde48e878e9
sohail019 <shaikhhsohail0193@gmail.com>
2024-08-26 17:41:31 +0530

    Learned about the new use API to read the value of context

--- 02 _Reactv19_By_Thapa/11_Other_Hooks/src/hooks/Custom Hook/useBioContext.js
+++ 02 _Reactv19_By_Thapa/11_Other_Hooks/src/hooks/Custom Hook/useBioContext.js
@@ -1,8 +1,12 @@
-import { useContext } from "react"
+// import { useContext } from "react"
+import {use} from "react"
 import { BioContext } from "../../contexts/Context"
 
 export const useBioContext = () =>{
+  //! useContext must be called at the top level of your component
+  // const context = useContext(BioContext)
 
-    const context = useContext(BioContext)
-    return context
+  // * use can be called inside conditionals like if and loops like for. use is preferred over useContext because it is more flexible.
+  const context = use(BioContext)
+  return context;
 } 
