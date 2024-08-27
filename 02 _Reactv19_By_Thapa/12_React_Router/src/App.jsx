ac8b15aaf15d9a213a2fccd7894e6625aaa4c987
sohail019 <shaikhhsohail0193@gmail.com>
2024-08-27 18:52:08 +0530

    Understood React router DOM, Created routes, Setup routes and created multiple pages and tested routes on browser

--- 02 _Reactv19_By_Thapa/12_React_Router/src/App.jsx
+++ 02 _Reactv19_By_Thapa/12_React_Router/src/App.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import { About } from './pages/About'
+import { Home } from './pages/Home'
 
 export const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    },
+    {
+      path: "about",
+      element: <About />
+    },
+
+  ])
+
   return (
-    <div>React Router</div>
+    <RouterProvider router={router}>
+
+    </RouterProvider>
   )
 }
