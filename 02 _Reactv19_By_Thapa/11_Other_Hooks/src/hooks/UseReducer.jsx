a8f0437d10be633b1933622f9b562dadcaaa23f8
sohail019 <shaikhhsohail0193@gmail.com>
2024-08-27 12:54:06 +0530

    Added Reset button, applied best practice for initial value, optimized with switch statement, manage objects as initial state and destructure properties of state

--- 02 _Reactv19_By_Thapa/11_Other_Hooks/src/hooks/UseReducer.jsx
+++ 02 _Reactv19_By_Thapa/11_Other_Hooks/src/hooks/UseReducer.jsx
@@ -4,22 +4,45 @@ export const UseReducer = () => {
   //! useState so far
   // const [count, setCount] = useState(0)
 
+  const initialState = {
+    count: 0,
+    inc: 2,
+    dec: 2
+  }
+
   const reducer = (state, action) => {
     console.log(action)
+    const { count, inc, dec } = state;
 
-    if(action.type === "INCREMENT") {
-      return state + 1
+    //! using if is not good approach
+    // if(action.type === "INCREMENT") {
+    //   return state + 1
+    // }
+    // if(action.type === "DECREMENT") {
+    //   return state - 1
+    // }
+    // if(action.type === "RESET"){
+    //   return state = 0
+    // }
+
+    //* we should use switch statement
+
+    switch(action.type) {
+      case "INCREMENT":
+        return {...state, count: count + inc}
+      case "DECREMENT":
+        return { ...state, count: count - dec };
+      case "RESET":
+        return { ...state, count: 0 };
     }
-    if(action.type === "DECREMENT") {
-      return state - 1
-    }
+
   }
 
   //* useReducer
-  const [count, dispatch] = useReducer(reducer, 0);
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="p-4 h-lvh flex flex-col justify-center items-center">
-      <h1>UseReducer : {count}</h1>
+      <h1>UseReducer : {state.count}</h1>
       <button
         onClick={() => dispatch({ type: "INCREMENT" })}
         className="bg-teal-500 hover:bg-teal-600 w-full text-white rounded-md mt-4 p-4"
@@ -31,6 +54,12 @@ export const UseReducer = () => {
         className="bg-teal-500 hover:bg-teal-600 w-full text-white rounded-md mt-4 p-4"
       >
         Decrement
+      </button>
+      <button
+        onClick={() => dispatch({ type: "RESET" })}
+        className="bg-teal-500 hover:bg-teal-600 w-full text-white rounded-md mt-4 p-4"
+      >
+        Reset
       </button>
     </div>
   );
