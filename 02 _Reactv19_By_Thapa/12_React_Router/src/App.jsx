355059223556f61a9cf380ec38dc0d61f951be52
sohail019 <shaikhhsohail0193@gmail.com>
2024-08-28 17:18:07 +0530

    Created Header and Footer Component, Built AppLayout Structure, Used Outlet Component to make it parent for all the pages

--- 02 _Reactv19_By_Thapa/12_React_Router/src/App.jsx
+++ 02 _Reactv19_By_Thapa/12_React_Router/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom"
+import { AppLayout } from './components/layout/AppLayout'
 import { About } from './pages/About'
 import { Contact } from './pages/Contact'
 import {Home} from "./pages/Home"
@@ -7,29 +8,42 @@ import { Movies } from './pages/Movies'
 
 export const App = () => {
 
-  // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: <Home />
-  //   },
-  //   {
-  //     path: "about",
-  //     element: <About />
-  //   },
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        {
+          path: "",
+          element: <Home />,
+        },
+        {
+          path: "about",
+          element: <About />,
+        },
+        {
+          path: "contact",
+          element: <Contact />,
+        },
+        {
+          path: "movies",
+          element: <Movies />,
+        },
+      ]
+    }
+  ]);
 
-  // ])
+  // const router = createBrowserRouter(
 
-  const router = createBrowserRouter(
-
-    createRoutesFromElements(
-      <>
-        <Route path='/' element={<Home />}/>
-        <Route path='about' element={<About />}/>
-        <Route path='contact' element={<Contact />}/>
-        <Route path='movies' element={<Movies />}/>
-      </>
-    )
-  )
+  //   createRoutesFromElements(
+  //     <>
+  //       <Route path='/' element={<Home />}/>
+  //       <Route path='about' element={<About />}/>
+  //       <Route path='contact' element={<Contact />}/>
+  //       <Route path='movies' element={<Movies />}/>
+  //     </>
+  //   )
+  // )
 
   return <RouterProvider router={router} />
   
